4bcf13dfee4f7eb9e60adda9a52eacc11ff1a9b2
Br3adFruit <56255988+Br3adFruit@users.noreply.github.com>
2020-02-24 12:27:05 +0000

    MainVrGame

---
+++ MainVR-Game/Assets/Characters/Enemies/Zlorp/Scripts/ZlorpMove.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZlorpMove : MonoBehaviour
+{
+    Animator animator;    
+    Transform playerTransform;
+    Transform zlorpTransform;
+    NavMeshAgent navMesh;
+    public float checkRate = 0.01f;
+    float nextCheck;
+    public float walkSpeed = 14;
+    public float runSpeed = 20;
+    
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        zlorpTransform = GameObject.FindGameObjectWithTag("Zlorp").transform;
+        navMesh = gameObject.GetComponent<NavMeshAgent>();        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateDistance();
+        Animations();
+       
+
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            Follow();
+        }           
+
+    }
+
+    void Follow()
+    {
+        navMesh.transform.LookAt(playerTransform);
+        navMesh.destination = playerTransform.position;
+    }
+
+    void Animations()
+    {
+        
+        if (navMesh.speed >= 16 && navMesh.speed > 15)
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("Walking", false);
+            animator.SetBool("Running", true);
+        }
+        else if (navMesh.speed < 15 && navMesh.speed > 0)
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("Walking", true);
+            animator.SetBool("Running", false);
+
+        }
+        else if (navMesh.speed <= 0)
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Walking", false);
+            animator.SetBool("Running", false);
+        }
+    }
+
+    void CalculateDistance()
+    {          
+        
+        float dist = Vector3.Distance(zlorpTransform.position, playerTransform.position);
+        //Debug.Log("The distance is.................." + dist);
+
+        if (dist >= 200)
+        {
+            navMesh.speed = 0;
+        }
+        else if (dist >= 151 && dist < 200)
+        {
+            navMesh.speed = walkSpeed;
+        }     
+        else if (dist <= 150 && dist >= 11)
+        {
+            navMesh.speed = runSpeed;
+        }
+        else
+        {
+            navMesh.speed = 0;           
+        }        
+    }
+
+}
