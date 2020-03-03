54517337bb6547db4ce2709a92c101c6720fae7d
glendelmundo <36743937+glendelmundo@users.noreply.github.com>
2020-03-03 19:42:32 +0000

    New SteamVR Version

---
+++ SteamVR Version/testtestvr/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 input;
+    public float speed = 1;
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+
+
+    // Update is called once per frame
+    private Vector3 direction;
+
+    void Update()
+    {
+        if (input.axis.magnitude > 0.1f)
+        {
+            direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (input.axis.magnitude > 0.1f)
+        {
+            characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
+        }
+    }
+}
