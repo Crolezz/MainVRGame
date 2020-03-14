8ccf5123d2afb9eb844a95b33ede46ffca95b9d6
Br3adFruit <56255988+Br3adFruit@users.noreply.github.com>
2020-03-14 23:48:27 +0000

    Enemy stuff
    . multiple enemy spawner
    . Playing with Animations

---
+++ SteamVR Version/testtestvr/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    Transform playerTransform;
+    NavMeshAgent navMesh;
+    public float checkRate = 0.01f;
+    float nextCheck;
+    Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        navMesh = gameObject.GetComponent<NavMeshAgent>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            Follow();
+        }
+
+        Animations();
+    }
+
+    void Follow()
+    {
+        navMesh.transform.LookAt(playerTransform);
+        navMesh.destination = playerTransform.position;
+    }
+
+    void Animations()
+    {
+        if(navMesh.speed > 0)
+        {
+            Anim.SetBool("Run", true);
+        }
+    }
+}
---
+++ SteamVR Version/testtestvr/Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemies; 
+    public GameObject[] spawnPoints;
+    public float startWait;
+    public float spawnWait;
+    public bool stop;
+    public Vector3 spawnValues;
+    public float Yvalue;
+    public float leastWait;
+    public float mostWait; 
+
+    int randEnemy; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range(leastWait, mostWait);
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(startWait); 
+
+        while(!stop)
+        {
+            randEnemy = Random.Range(0,2); 
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0, Random.Range(-spawnValues.z, spawnValues.z));
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
