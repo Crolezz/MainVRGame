e2310c92b0f2cf0722223a993bf5325dd9b2f114
Br3adFruit <56255988+Br3adFruit@users.noreply.github.com>
2020-03-17 02:36:40 +0000

    Fixed Flying Enemy
    . Flying Enemies follow player and shoot when in range
    .  Revised Spawner script
    . Navmesh revised

---
+++ SteamVR Version/testtestvr/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+    int randEnemy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        while(enemyCount < 10)
+        {
+            randEnemy = Random.Range(0, 2);
+            xPos = Random.Range(-75, 75);
+            zPos = Random.Range(-95, 95);
+            Instantiate(enemies[randEnemy], new Vector3(xPos,20, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+            enemyCount += 1; 
+        }
+       
+
+    }
+}
---
+++ SteamVR Version/testtestvr/Assets/Scripts/FlyingEnemyFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+
+public class FlyingEnemyFollow : MonoBehaviour
+{
+    GameObject Target;
+    private NavMeshAgent nma; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = this.GetComponent<NavMeshAgent>();
+        Target = GameObject.FindGameObjectWithTag("MainCamera"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        nma.SetDestination(Target.transform.position);    
+    }
+}
